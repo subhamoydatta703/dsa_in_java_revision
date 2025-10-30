e8643a22325a936fdd97222315b35cb2a1967ef6
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-10-30 12:36:47 +0530

    Sum of evens and odds

--- loops.java
+++ loops.java
@@ -1,8 +1,27 @@
 public class loops {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Hello");
-            i += 2;
+        // for (int i = 0; i < 5; i++) {
+        //     System.out.println("Hello");
+        //     i += 2;
+        // }
+
+        // int arr[]= new int[5];
+        // int[] arr; 
+        // int arr[] = {1, 2, 3, 4, 5};
+
+        int arr[] = {1, 2, 3, 4, 5};
+        int sumOdd = 0, sumEven=0;
+        for(int i=0; i<arr.length;i++){
+            if(arr[i]%2==0){
+                sumEven+=arr[i];
+
+            }else{
+                sumOdd+= arr[i];
+            }
         }
+        System.out.print("Sum of evens: "+ sumEven);
+        System.out.println();
+        System.out.print("Sum of odds: "+ sumOdd);
+
     }
 }
