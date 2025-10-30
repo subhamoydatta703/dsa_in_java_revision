d752a750e33387db346184daf3cb47c4e90cb6d4
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-10-30 11:26:13 +0530

    Number checking

--- conditional_stat.java
+++ conditional_stat.java
@@ -1,23 +1,39 @@
 import java.util.*;
 public class conditional_stat {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the income: ");
-        int num = sc.nextInt();
-        if(num<=1200000){
-            System.out.print("No tax");
-        }else{
-            if(num<=1500000){
-                System.out.println("Your tax will be 20%");
-                System.out.print("Your tax will be:"+ (num*0.2));
 
-            }
-            else{
-                System.out.println("Your tax will be 30%");
-                System.out.print("Your tax will be:"+ (num*0.3));
-            }
+    // Checking a number is positive or negative
+    static void checkNum(int n){
+        if(n==0){
+            System.out.print("It's zero");
         }
-        sc.close();
+        else if(n<0){
+            System.out.println("Its a negative number");
+        }
+        else{
+            System.out.print("It's a positive number");
+        }
+
+    }
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the income: ");
+        // int num = sc.nextInt();
+        // if(num<=1200000){
+        //     System.out.print("No tax");
+        // }else{
+        //     if(num<=1500000){
+        //         System.out.println("Your tax will be 20%");
+        //         System.out.print("Your tax will be:"+ (num*0.2));
+
+        //     }
+        //     else{
+        //         System.out.println("Your tax will be 30%");
+        //         System.out.print("Your tax will be:"+ (num*0.3));
+        //     }
+        // }
+        // sc.close();
+
+        checkNum(-9);
 
         
     }
