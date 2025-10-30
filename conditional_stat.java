c883827012d68892de0788eef454f4dd1cb15b0f
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-10-30 12:12:45 +0530

    Practice question

--- conditional_stat.java
+++ conditional_stat.java
@@ -83,7 +83,12 @@ public class conditional_stat {
         // checkNum(-9);
 
         // feverCheck(96);
-        weekCheck(4);
+        // weekCheck(4);
+        int a=63,b=36;
+        boolean x= (a<b)?true:false;
+        int y= (a>b)?a:b;
+        System.out.println(x);
+        System.out.println(y);
 
     }
 }
