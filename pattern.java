92882b0f9b2dc046e9457e3b4268cf2b99c0e2cc
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-10-31 21:01:33 +0530

    Star pattern

---
+++ pattern.java
@@ -0,0 +1,16 @@
+public class pattern {
+
+    // star print
+    static void star(int n){
+        for(int i=1; i<=n; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        star(5);
+    }
+}
