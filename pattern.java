46b36da61cf1dbf407b1aa2679be8d9fd652730e
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-10-31 21:13:48 +0530

    Inverted stars

--- pattern.java
+++ pattern.java
@@ -10,7 +10,16 @@ public class pattern {
         }
     }
 
+    // inverted star print
+    static void invStar(int n){
+        for(int i=1; i<=n; i++){
+            for(int j = 1; j<=n-i+1; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        star(5);
+        invStar(5);
     }
 }
