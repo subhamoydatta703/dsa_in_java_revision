a516cf74c3dde1e4aea7d69231db277708e48c1b
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-10-31 22:09:42 +0530

    Character Pattern

--- pattern.java
+++ pattern.java
@@ -1,9 +1,9 @@
 public class pattern {
 
     // star print
-    static void star(int n){
-        for(int i=1; i<=n; i++){
-            for(int j = 1; j<=i; j++){
+    static void star(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
@@ -11,15 +11,41 @@ public class pattern {
     }
 
     // inverted star print
-    static void invStar(int n){
-        for(int i=1; i<=n; i++){
-            for(int j = 1; j<=n-i+1; j++){
+    static void invStar(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
     }
+
+    // half pyramid
+
+    static void halfPyr(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // character pattern
+    static void charPat(int n) {
+        char ch = 'A';
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        invStar(5);
+        // invStar(5);
+        // halfPyr(4);
+        charPat(5);
     }
 }
