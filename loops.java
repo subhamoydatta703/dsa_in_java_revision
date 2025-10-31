dd63b0ec0ea8b303bd6df969e8f8fc6c4da29f13
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-10-31 08:36:09 +0530

    adding comments

--- loops.java
+++ loops.java
@@ -1,27 +1,27 @@
 public class loops {
     public static void main(String[] args) {
         // for (int i = 0; i < 5; i++) {
-        //     System.out.println("Hello");
-        //     i += 2;
+        // System.out.println("Hello");
+        // i += 2;
         // }
 
         // int arr[]= new int[5];
-        // int[] arr; 
+        // int[] arr;
         // int arr[] = {1, 2, 3, 4, 5};
 
-        int arr[] = {1, 2, 3, 4, 5};
-        int sumOdd = 0, sumEven=0;
-        for(int i=0; i<arr.length;i++){
-            if(arr[i]%2==0){
-                sumEven+=arr[i];
+        int arr[] = { 1, 2, 3, 4, 5 };
+        int sumOdd = 0, sumEven = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                sumEven += arr[i];
 
-            }else{
-                sumOdd+= arr[i];
+            } else {
+                sumOdd += arr[i];
             }
         }
-        System.out.print("Sum of evens: "+ sumEven);
+        System.out.print("Sum of evens: " + sumEven);
         System.out.println();
-        System.out.print("Sum of odds: "+ sumOdd);
+        System.out.print("Sum of odds: " + sumOdd);
 
     }
 }
