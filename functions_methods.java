895a46379378733272b5fa29def6b42ddcde070f
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-11-01 14:56:22 +0530

    Products and factorial

--- functions_methods.java
+++ functions_methods.java
@@ -11,9 +11,25 @@ public class functions_methods {
         name();
     }
 
+    // products of 2 numbers
+    static void prod(int x, int y){
+        System.out.print(x*y);
+    }
+
+    // factorial using loop
+    static int fac(int n){
+        int fact=1;
+        for(int i=1; i<=n; i++){
+            fact = fact * i;
+        }
+        return fact;
+    }
+
     public static void main(String[] args) {
-        System.out.println("This is main()");
-        greet();
+        // System.out.println("This is main()");
+        // greet();
+        // prod(2,3);
+        // System.out.print(fac(4));
         
     }
 }
