dcd8d1d9f49b8154574fa3456613d65abf7caa96
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-11-02 14:08:07 +0530

    Method overloading in java

--- functions_methods.java
+++ functions_methods.java
@@ -1,25 +1,26 @@
 public class functions_methods {
     // call stack
 
-    static void name(){
+    static void name() {
         System.out.println("This is name()");
         System.out.println("Your name");
     }
-    static void greet(){
+
+    static void greet() {
         System.out.println("This is greet()");
         System.out.println("Hello");
         name();
     }
 
     // products of 2 numbers
-    static void prod(int x, int y){
-        System.out.print(x*y);
+    static void prod(int x, int y) {
+        System.out.print(x * y);
     }
 
     // factorial using loop
-    static int fac(int n){
-        int fact=1;
-        for(int i=1; i<=n; i++){
+    static int fac(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
             fact = fact * i;
         }
         return fact;
@@ -27,13 +28,13 @@ public class functions_methods {
 
     // prime number check
 
-    static void primeNum(int n){
-        if(n<=1){
+    static void primeNum(int n) {
+        if (n <= 1) {
             System.out.print("Not Prime");
             return;
         }
-        for(int i =2; i<n; i++){
-            if(n%i==0){
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
                 System.out.print(" Not Prime");
                 return;
             }
@@ -43,12 +44,29 @@ public class functions_methods {
 
     // Binomial coefficient
 
-    static int binCoeff(int n, int r){
-        int facN= fac(n);
-        int facR= fac(r);
-        int x = (n-r);
+    static int binCoeff(int n, int r) {
+        int facN = fac(n);
+        int facR = fac(r);
+        int x = (n - r);
         int down = facR * fac(x);
-        return facN/down;
+        return facN / down;
+    }
+
+    // function(method in java) overloading
+
+    // Method 1: Adds two integers
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method 2: Adds three integers
+    static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Method 3: Adds two double values
+    static double add(double a, double b) {
+        return a + b;
     }
 
     public static void main(String[] args) {
@@ -57,7 +75,10 @@ public class functions_methods {
         // prod(2,3);
         // System.out.print(fac(4));
         // System.out.print(binCoeff(5, 2));
-        primeNum(7);
-        
+        // primeNum(7);
+        System.out.println("Sum of 2 ints: " + add(5, 10));
+        System.out.println("Sum of 3 ints: " + add(5, 10, 15));
+        System.out.println("Sum of 2 doubles: " + add(5.5, 10.2));
+
     }
 }
