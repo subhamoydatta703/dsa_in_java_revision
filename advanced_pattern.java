5b16228b7ab5c32b52590a998b26d51b3ef13f4f
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-11-04 20:43:32 +0530

    Hollow rectangle pattern

---
+++ advanced_pattern.java
@@ -0,0 +1,19 @@
+public class advanced_pattern {
+
+    static void hollow(int r, int c){
+        for(int i =1; i<=r; i++){
+            for(int j =1; j<=c; j++){
+                if(i==1 || i==r || j ==1  || j == c){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+    public static void main(String[] args) {
+        hollow(4, 5);
+    }
+}
