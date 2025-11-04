5bc1b999ddb49b43af160d0f837bad1f23d1d8b7
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-11-04 11:35:02 +0530

    Palindrome checking method

--- practice.java
+++ practice.java
@@ -1,19 +1,20 @@
 public class practice {
-// Method overloading
+    // Method overloading
 
-    static int sum(int x, int y){
-        return x+y;
-    } 
-    
-static float sum(float x, float y){
-    return x+y;
-}
+    static int sum(int x, int y) {
+        return x + y;
+    }
 
-static void avg(float a, float b, float c){
-    float AVG = (a+b+c)/3;
-    System.out.print(AVG);
-}
-// Reverse string:
+    static float sum(float x, float y) {
+        return x + y;
+    }
+
+    static void avg(float a, float b, float c) {
+        float AVG = (a + b + c) / 3;
+        System.out.print(AVG);
+    }
+
+    // Reverse string:
     static void reverse(String str) {
         String rev = "";
         for (int i = str.length() - 1; i >= 0; i--) {
@@ -22,21 +23,38 @@ static void avg(float a, float b, float c){
         System.out.print(rev);
     }
 
-    static boolean isEven(int x){
-        if(x%2==0){
+    static boolean isEven(int x) {
+        if (x % 2 == 0) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
+
+    static void palindrome(int n) {
+        int orgNum = n;
+        int rev = 0;
+        while (n > 0) {
+            int lastDig = n % 10;
+
+            n = n / 10;
+            rev = rev * 10 + lastDig;
+        }
+        if (orgNum == rev) {
+            System.out.print("Palondrome");
+        } else {
+            System.out.print("Not a palindrome");
+        }
+    }
+
     public static void main(String[] args) {
-        
+
         // System.out.println(sum(6, 4));
         // System.out.println(sum(3.33f, 5.55f));
 
         // reverse("JAVA");
         // avg(3, 5, 4);
-        System.out.print(isEven(5));
+        palindrome(121);
 
     }
 }
