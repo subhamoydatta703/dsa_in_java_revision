0bfd6fd2561ecfe0a4591f91706c489cbe4ac5f9
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-12-11 12:37:51 +0530

    "Linear search"

--- arrays/linear_search.java
+++ arrays/linear_search.java
@@ -1,5 +1,20 @@
 package arrays;
+import java.util.*;
 
 public class linear_search {
-    
+
+    static void findVal(int x, int arr[]) {
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == x) {
+                System.out.println("Index: " + i + " , Value: " + x);
+                return;  // exit function after finding
+            }
+        }
+        System.out.println("Not found");
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 4, 8, 9};
+        findVal(9, arr);
+    }
 }
