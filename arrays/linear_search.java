c989201d7ac9e8aa613eb9067eb61f78cbef43ea
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-12-11 12:47:03 +0530

    "Linear search by using index"

--- arrays/linear_search.java
+++ arrays/linear_search.java
@@ -3,18 +3,37 @@ import java.util.*;
 
 public class linear_search {
 
-    static void findVal(int x, int arr[]) {
+    // linear search by using value
+    static void linSearchByVal(int x, int arr[]) {
         for(int i = 0; i < arr.length; i++) {
             if(arr[i] == x) {
                 System.out.println("Index: " + i + " , Value: " + x);
-                return;  // exit function after finding
+                return;
             }
         }
         System.out.println("Not found");
     }
 
+
+    // linear search by using index
+    static void linSearchByIndx(int y, int arr[]){
+        if(y>=arr.length || y<0){
+            System.out.print("Index should be within 0 to "+(arr.length-1));
+            return;
+        }
+        for(int i =0; i <arr.length; i++){
+            if( i ==y){
+                System.out.print("Value is: "+ arr[i]+" at index: "+ y);
+            return;
+            }
+            
+        }
+    }
+
+
     public static void main(String[] args) {
         int arr[] = {1, 2, 4, 8, 9};
-        findVal(9, arr);
+        // linSearchByVal(9, arr);
+        linSearchByIndx(3, arr);
     }
 }
