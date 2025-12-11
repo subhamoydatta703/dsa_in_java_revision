9ae3f240e8691c2a41be77c562a0b0899805d3ca
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-12-11 15:26:54 +0530

    "Sub arrays"

---
+++ arrays/sub_arrays.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class sub_arrays {
+
+    // sub arrays
+    static void subArrays(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            int start = i;
+            for (int j = i; j < arr.length; j++) {
+                int end = j;
+                for (int k = start; k <= end; k++) {
+                    System.out.print(arr[k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 2, 4, 6, 8, 10 };
+        subArrays(arr);
+    }
+}
