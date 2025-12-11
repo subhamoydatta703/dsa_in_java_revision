1cef826d7f00fbbb68d02bf6c22f7db3200155d1
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-12-11 13:06:21 +0530

    "Linear search and maximum value"

--- arrays/linear_search.java
+++ arrays/linear_search.java
@@ -1,12 +1,13 @@
 package arrays;
+
 import java.util.*;
 
 public class linear_search {
 
     // linear search by using value
     static void linSearchByVal(int x, int arr[]) {
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] == x) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
                 System.out.println("Index: " + i + " , Value: " + x);
                 return;
             }
@@ -14,25 +15,23 @@ public class linear_search {
         System.out.println("Not found");
     }
 
-
     // linear search by using index
-    static void linSearchByIndx(int y, int arr[]){
-        if(y>=arr.length || y<0){
-            System.out.print("Index should be within 0 to "+(arr.length-1));
+    static void linSearchByIndx(int y, int arr[]) {
+        if (y >= arr.length || y < 0) {
+            System.out.print("Index should be within 0 to " + (arr.length - 1));
             return;
         }
-        for(int i =0; i <arr.length; i++){
-            if( i ==y){
-                System.out.print("Value is: "+ arr[i]+" at index: "+ y);
-            return;
+        for (int i = 0; i < arr.length; i++) {
+            if (i == y) {
+                System.out.print("Value is: " + arr[i] + " at index: " + y);
+                return;
             }
-            
+
         }
     }
 
-
     public static void main(String[] args) {
-        int arr[] = {1, 2, 4, 8, 9};
+        int arr[] = { 1, 2, 4, 8, 9 };
         // linSearchByVal(9, arr);
         linSearchByIndx(3, arr);
     }
---
+++ arrays/max_value_find.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class max_value_find {
+    // maximum value find in an array
+    static void findMax(int arr[]) {
+        int x = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > x) {
+                x = arr[i];
+            }
+        }
+        System.out.print("Maximum value is: " + x);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 10, 20, 15, 36, 28 };
+        findMax(arr);
+
+    }
+
+}
