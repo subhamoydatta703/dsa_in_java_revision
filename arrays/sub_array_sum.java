46503290c7174ddcda8475659ed6d8894e8ccc6c
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-12-14 22:22:45 +0530

    "Maximum sub array sum by prefix sum"

--- arrays/sub_array_sum.java
+++ arrays/sub_array_sum.java
@@ -2,8 +2,8 @@ package arrays;
 
 public class sub_array_sum {
 
-    // sub array sum
-    static void subArraySum(int arr[]) {
+    // sub array sum by bruteforce
+    static void bruteForceSubArraySum(int arr[]) {
         int currSum = 0, maxSum = Integer.MIN_VALUE;
         // int sum =0;
         for (int i = 0; i < arr.length; i++) {
@@ -24,9 +24,35 @@ public class sub_array_sum {
         System.out.println(maxSum);
     }
 
+
+    // prefix sub array sum
+    static void prefixSubArraySum(int arr[]) {
+        int currSum = 0, maxSum = Integer.MIN_VALUE;
+        int prefix[]= new int[arr.length];
+        // calculation of prefix sum
+        prefix[0]= arr[0];
+        for(int a =1; a<prefix.length;a++){
+            prefix[a]= prefix[a-1]+arr[a];
+        }
+        
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                currSum = i==0 ?  prefix[j] : prefix[j]-prefix[i-1];
+                 if (currSum > maxSum) {
+                maxSum = currSum;
+            }
+                // System.out.println();
+            }
+            
+            // System.out.println();
+        }
+        System.out.println(maxSum);
+    }
+
     public static void main(String[] args) {
         int arr[] = { 2, 3, 4, 5, 6 };
-        subArraySum(arr);
+        // bruteForceSubArraySum(arr);
+        prefixSubArraySum(arr);
     }
 
 }
