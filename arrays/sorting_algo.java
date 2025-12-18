23460abd8363c7ea0dcd6204b7bd1f6003d35817
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-12-18 23:50:41 +0530

    "Bubble sort"

---
+++ arrays/sorting_algo.java
@@ -0,0 +1,29 @@
+package arrays;
+
+import java.util.*;
+
+public class sorting_algo {
+
+    // bubble sort
+
+    static int[] bubbleSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i <= (n - 2); i++) {
+            for (int j = 0; j < (n - i - 1); j++) {
+                if (arr[j + 1] < arr[j]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+        }
+        return arr;
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 7, 2, 1, 3 };
+        System.out.print(Arrays.toString(bubbleSort(arr)));
+    }
+}
