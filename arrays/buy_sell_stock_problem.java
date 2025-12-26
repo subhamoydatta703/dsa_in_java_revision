71356c7c6b5b5d427d0409d251040698eb16597c
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-12-27 00:54:11 +0530

    "Buy and sale of stocks problem"

---
+++ arrays/buy_sell_stock_problem.java
@@ -0,0 +1,21 @@
+package arrays;
+import java.util.*;
+public class buy_sell_stock_problem {
+    static int buyAndSellStocks(int prices[]){
+        int buyPrice = Integer.MAX_VALUE, maxProfit=0;
+        for(int i =0; i<prices.length; i++){
+            if(buyPrice< prices[i]){
+                int profit = prices[i]-buyPrice;
+                maxProfit= Math.max(maxProfit, profit);
+            }
+            else{
+            buyPrice = prices[i];
+        }
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int arr[]= {7,1,5,3,6,4};
+        System.out.print(buyAndSellStocks(arr));
+    }
+}
