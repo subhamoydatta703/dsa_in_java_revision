1ab101b57fc6782c4eae6f2164d7cafb87d61ea5
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-12-28 00:40:26 +0530

    "Selection sort"

--- arrays/sorting_algo.java
+++ arrays/sorting_algo.java
@@ -20,9 +20,27 @@ public class sorting_algo {
         return arr;
 
     }
+    // selection sort
+
+    static int[] selectionSort(int arr[]){
+        int n = arr.length;
+        for(int i=0; i<n-1; i++){
+            int minValuePos = i;
+            for(int j = i+1; j<n; j++){
+                if(arr[minValuePos]> arr[j]){
+                    minValuePos = j;
+                }
+            }
+            int temp = arr[minValuePos];
+            arr[minValuePos] = arr[i];
+            arr[i]= temp;
+        }
+        return arr;
+    }
 
     public static void main(String[] args) {
-        int arr[] = { 5, 7, 2, 1, 3 };
-        System.out.print(Arrays.toString(bubbleSort(arr)));
+        int arr[] = { 5, 7, 3, 1, 2 };
+        // System.out.print(Arrays.toString(bubbleSort(arr)));
+        System.out.print(Arrays.toString(selectionSort(arr)));
     }
 }
