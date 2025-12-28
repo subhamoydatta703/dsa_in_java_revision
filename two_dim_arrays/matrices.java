d66867333e1a4967e49cfc2d184c19415eb6d817
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-12-29 00:08:22 +0530

    "Example of a 2d array"

--- two_dim_arrays/matrices.java
+++ two_dim_arrays/matrices.java
@@ -5,12 +5,20 @@ import java.util.Scanner;
 public class matrices {
     public static void main(String[] args) {
         int matrix[][]= new int[3][3];
-        int n = matrix.length,   m = matrix[0].length; 
+        int n = matrix.length, m = matrix[0].length;
         Scanner sc = new Scanner(System.in);
+        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
+                System.out.print("Enter the value of row: "+(i+1)+" and Column: "+(j+1)+" : ");
                 matrix[i][j]= sc.nextInt();
-                System.out.print(matrix[i][j]);
+            }
+        
+            System.out.println();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j]+" ");
             }
             System.out.println();
         }
