fc93eab210c83e4bdd2b76fcacc39e47957897f2
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-12-29 15:55:41 +0530

    "Matrix diagonal sum problem"

---
+++ two_dim_arrays/matrix_diagonal_sum.java
@@ -0,0 +1,42 @@
+package two_dim_arrays;
+
+public class matrix_diagonal_sum {
+    // brute force
+    static int diag_sum(int mat[][]) {
+        int sum = 0;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (i == j) {
+                    sum += mat[i][j];
+
+                } else if (i + j == mat.length - 1) {
+                    sum += mat[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+
+    // optimized
+    static int diagonalSum(int[][] mat) {
+        int pd = 0, sd = 0, n = mat.length;
+        for (int i = 0; i < n; i++) {
+            pd += mat[i][i];
+            if (i != n - i - 1) {
+                sd += mat[i][n - i - 1];
+            }
+        }
+        return pd + sd;
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = {
+                { 1, 2, 3 },
+                { 5, 6, 7 },
+                { 9, 10, 11 }
+        };
+        // System.out.print(diag_sum(matrix));
+        System.out.print(diagonalSum(matrix));
+
+    }
+}
