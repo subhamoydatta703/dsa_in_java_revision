d07b67bce9661b96edfcb2118bc1a6aaa29e1149
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-12-29 11:27:46 +0530

    "Searching a key value in the matrix"

--- two_dim_arrays/matrices.java
+++ two_dim_arrays/matrices.java
@@ -3,6 +3,19 @@ package two_dim_arrays;
 import java.util.Scanner;
 
 public class matrices {
+    // search in 2d array
+    static boolean isFind(int arr[][], int key){
+        int n = arr.length, m= arr[0].length;
+         for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(arr[i][j]==key){
+                    System.out.print("Found at index: ("+(i+1)+", "+(j+1)+")");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         int matrix[][]= new int[3][3];
         int n = matrix.length, m = matrix[0].length;
@@ -16,11 +29,13 @@ public class matrices {
         
             System.out.println();
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(matrix[i][j]+" ");
-            }
-            System.out.println();
-        }
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < m; j++) {
+        //         System.out.print(matrix[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
+        System.out.print(isFind(matrix, 10));
+
     }
 }
