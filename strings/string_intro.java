fbafe43aad1f3aeda3d25055158bf3b8ba34d619
Subhamoy Datta <subhamoydatta99@gmail.com>
2026-01-04 15:07:42 +0530

    "String introduction"

---
+++ strings/string_intro.java
@@ -0,0 +1,14 @@
+package strings;
+
+public class string_intro {
+    public static void main(String[] args) {
+        // array of characters
+        int[] ch = {'a','b','c','d'};
+        // string declare(method 1)
+        String str1 = "abcdef";
+        // string declare(method 2: with new keyword)
+        String str2 = new String("ghijkl");
+        // String is a built-in class in java for Strings
+        // Strings are immutable(can not modify the original string)
+    }
+}
