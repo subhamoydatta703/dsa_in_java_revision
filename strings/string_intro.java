6c14675eb466e0d5d2dccf9bf89946cb75f3cf92
Subhamoy Datta <subhamoydatta99@gmail.com>
2026-01-04 15:13:05 +0530

    "Using Scanner with string"

--- strings/string_intro.java
+++ strings/string_intro.java
@@ -1,5 +1,7 @@
 package strings;
 
+import java.util.Scanner;
+
 public class string_intro {
     public static void main(String[] args) {
         // array of characters
@@ -10,5 +12,14 @@ public class string_intro {
         String str2 = new String("ghijkl");
         // String is a built-in class in java for Strings
         // Strings are immutable(can not modify the original string)
+
+        // USing input
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        // next() -> not allows words/letters after space
+        String name = sc.next();
+        // nextLine() -> allows words/letters after space until press enter
+        // String name = sc.nextLine(); 
+        System.out.print("Hello "+name);
     }
 }
