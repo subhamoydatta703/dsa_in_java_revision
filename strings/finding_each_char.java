4b4aa18b2916eb626590243e7ddcf936e6562b55
Subhamoy Datta <subhamoydatta99@gmail.com>
2026-01-04 17:27:01 +0530

    "Printing all characters of a string"

---
+++ strings/finding_each_char.java
@@ -0,0 +1,15 @@
+package strings;
+
+public class finding_each_char {
+    // finding each characters of a string using charAt() method
+    static void eachChar(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println("Character at index: " + i + " is: " + str.charAt(i));
+        }
+    }
+
+    public static void main(String[] args) {
+        String str1 = "I am a developer.";
+        eachChar(str1);
+    }
+}
