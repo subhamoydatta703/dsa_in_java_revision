05964fc1931603e21cb35eba4fcad8449366843c
Subhamoy Datta <subhamoydatta99@gmail.com>
2026-01-04 18:04:39 +0530

    "Different approaches to find palindrome string"

--- strings/palindrome_check.java
+++ strings/palindrome_check.java
@@ -1,6 +1,19 @@
 package strings;
 
 public class palindrome_check {
+    // 1st approach(good)
+    static boolean isPalindromeString(String s) {
+        int n = s.length() - 1;
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(n)) {
+                return false;
+            }
+            n--;
+        }
+        return true;
+    }
+
+    // 2nd approach(better)
     static boolean isPalindrome(String s) {
         int l = 0, r = s.length() - 1, n = s.length();
         while (l < r) {
@@ -13,14 +26,15 @@ public class palindrome_check {
         }
         return true;
     }
-    // 2nd approach
-    static boolean isPalindromeString(String s){
-        int n=s.length()-1;
-        for(int i=0; i<s.length()/2; i++){
-            if(s.charAt(i) != s.charAt(n)){
+
+    // 3rd approach(best)
+    static boolean isPalindromeStr(String s) {
+        int n = s.length();
+        for (int i = 0; i < n / 2; i++) {
+            if (s.charAt(i) != s.charAt(n - i - 1)) {
                 return false;
             }
-            n--;
+
         }
         return true;
     }
@@ -28,6 +42,8 @@ public class palindrome_check {
     public static void main(String[] args) {
         String str = "MADAM";
         // System.out.print(isPalindrome(str));
-        System.out.println(isPalindromeString(str));
+        // System.out.println(isPalindromeString(str));
+        System.out.println(isPalindromeStr(str));
+
     }
 }
