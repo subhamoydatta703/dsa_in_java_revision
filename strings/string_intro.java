c055bfdd147c1a65043ed15251e6458604cb78dd
Subhamoy Datta <subhamoydatta99@gmail.com>
2026-01-04 15:22:02 +0530

    "Finding string length"

--- strings/string_intro.java
+++ strings/string_intro.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class string_intro {
     public static void main(String[] args) {
         // array of characters
-        int[] ch = {'a','b','c','d'};
+        int[] ch = { 'a', 'b', 'c', 'd' };
         // string declare(method 1)
         String str1 = "abcdef";
         // string declare(method 2: with new keyword)
@@ -19,7 +19,23 @@ public class string_intro {
         // next() -> not allows words/letters after space
         String name = sc.next();
         // nextLine() -> allows words/letters after space until press enter
-        // String name = sc.nextLine(); 
-        System.out.print("Hello "+name);
+        // String name = sc.nextLine();
+        System.out.print("Hello " + name);
+        sc.close();
+
+        // length of a string check
+
+        // method 1-> by using a loop
+        String s = "Code";
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            count++;
+            System.out.println(c);
+        }
+
+        System.out.println("Length = " + count);
+        // method 2 -> using build in method -> str.length();
+        String str = "Java";
+        System.out.print("String length: " + str.length());
     }
 }
