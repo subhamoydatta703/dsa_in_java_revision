5bd91f367282c812a3ad720f84691a1ed60bfa5d
Subhamoy Datta <subhamoydatta99@gmail.com>
2026-01-11 14:23:38 +0530

    "String compression"

---
+++ strings/string_compression.java
@@ -0,0 +1,27 @@
+package strings;
+
+public class string_compression {
+    static String compress(String chars) {
+        StringBuilder sb = new StringBuilder("");
+
+        for (int i = 0; i < chars.length(); i++) {
+            Integer count = 1;
+            while (i < chars.length() - 1 && chars.charAt(i) == chars.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+
+            sb.append(chars.charAt(i));
+            if (count > 1) {
+                sb.append(count);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "aaabbcc";
+        System.out.print(compress(str));
+
+    }
+}
