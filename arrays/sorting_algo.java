e478e1bcf27781635799a8ad69fbbdbe50909435
Subhamoy Datta <subhamoydatta99@gmail.com>
2026-01-12 13:23:36 +0530

    "Quick sort algorithm"

--- arrays/sorting_algo.java
+++ arrays/sorting_algo.java
@@ -4,6 +4,13 @@ import java.util.*;
 
 public class sorting_algo {
 
+    // display array
+    static void displayArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
     // bubble sort
     static int[] bubbleSort(int arr[]) {
         int n = arr.length;
@@ -51,11 +58,51 @@ public class sorting_algo {
         }
         return arr;
     }
+    // quick sort
+
+    static int partition(int[] arr, int low, int high) {
+
+        int pivot = arr[high];
+        int i = low;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+
+                i++;
+            }
+        }
+
+        // place pivot at index i
+        int temp = arr[i];
+        arr[i] = arr[high];
+        arr[high] = temp;
+
+        return i;
+    }
+
+    static void quickSort(int[] arr, int low, int high) {
+
+        if (low < high) {
+
+            int pivotIndex = partition(arr, low, high);
+
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+
+    }
 
     public static void main(String[] args) {
         int arr[] = { 5, 7, 3, 1, 2 };
         // System.out.print(Arrays.toString(bubbleSort(arr)));
         // System.out.print(Arrays.toString(selectionSort(arr)));
-        System.out.print(Arrays.toString(insertionSort(arr)));
+        // System.out.print(Arrays.toString(insertionSort(arr)));
+        quickSort(arr, 0, 4);
+        displayArray(arr);
     }
 }
