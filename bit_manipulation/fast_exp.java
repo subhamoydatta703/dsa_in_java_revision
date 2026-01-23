7dd5515a711d2c455bcb721c2a94ffd262828804
Subhamoy Datta <subhamoydatta99@gmail.com>
2026-01-23 11:10:58 +0530

    Fast Exponentiation

---
+++ bit_manipulation/fast_exp.java
@@ -0,0 +1,23 @@
+package bit_manipulation;
+
+public class fast_exp {
+    static double fastExp(double x, long n) {
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        double ans = 1;
+        while (n > 0) {
+            if ((n & 1) != 0) {
+                ans *= x;
+            }
+            x *= x;
+            n = n >> 1;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.print(fastExp(2, -4));
+    }
+}
