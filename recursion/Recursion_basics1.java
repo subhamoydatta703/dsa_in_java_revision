e94d13f3436a4b5e27e6974c521fe0988a8c515a
Subhamoy Datta <subhamoydatta99@gmail.com>
2026-02-06 22:36:55 +0530

    Some problems

--- recursion/Recursion_basics1.java
+++ recursion/Recursion_basics1.java
@@ -6,9 +6,7 @@ public class Recursion_basics1 {
     static void nToOne(int n) {
         if (n == 0)
             return;
-
         System.out.print(n + " ");
-        ;
         nToOne(n - 1);
 
     }
@@ -36,10 +34,30 @@ public class Recursion_basics1 {
         return n + sumOfNatNum(n - 1);
     }
 
+    // fibonacci of n
+    static int fib(int n) {
+        if (n == 0 || n == 1)
+            return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    // checking array is sorted or not
+
+    static boolean isSorted(int[] arr, int i) {
+        if ((arr.length - 1) == i)
+            return true;
+        if (arr[i] > arr[i + 1])
+            return false;
+        return isSorted(arr, i + 1);
+    }
+
     public static void main(String[] args) {
+        int[] arr = { 1, 2, 4, 3, 5 };
         // nToOne(10);
         // oneTon(5);
         // System.out.print(fact(4));
-        System.out.println(sumOfNatNum(5));
+        // System.out.println(sumOfNatNum(5));
+        // System.out.print(fib(16));
+        System.out.println(isSorted(arr, 0));
     }
 }
