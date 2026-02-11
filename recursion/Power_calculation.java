f792bf7ead080f9077a63ee2b14d6e0382411aa3
Subhamoy Datta <subhamoydatta99@gmail.com>
2026-02-11 17:00:44 +0530

    Power calculation

---
+++ recursion/Power_calculation.java
@@ -0,0 +1,28 @@
+package recursion;
+
+public class Power_calculation {
+    // method 1
+    static int power(int x, int n) {
+        if (n == 0)
+            return 1;
+        return power(x, n - 1) * x;
+    }
+
+    // method 2(optimized)
+    static int optPower(int x, int n) {
+        if (n == 0)
+            return 1;
+        int halfPow = optPower(x, n / 2) * optPower(x, n / 2);
+
+        if (n % 2 != 0) {
+            halfPow = x * halfPow;
+        }
+
+        return halfPow;
+    }
+
+    public static void main(String[] args) {
+        // System.out.println(power(2, 3));
+        System.out.println(optPower(2, 5));
+    }
+}
